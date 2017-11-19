863c9c8272b1cf7b89de5d3c9caf29a38195a178
Avneesh Sarwate <avneeshsarwate@gmail.com>
2017-11-19 14:05:49 -0500

    updated feedback circle texture for webcam

--- The_Force-gh-pages/forceCode/feedbackBlendOSCRotation.glsl
+++ The_Force-gh-pages/forceCode/feedbackBlendOSCRotation.glsl
@@ -10,8 +10,7 @@ void main () {
     vec2 stN = uvN();
     vec2 st = uv();
     
-    vec3 t1 = texture2D(channel0, stN.xy).rgb;
-    vec3 t2 = texture2D(channel1, stN.xy).rgb;
+    vec3 t1 = texture2D(channel0, stN.xy).rgb; //webcam texture
     vec3 bb = texture2D(backbuffer, stN.xy).rgb;
     vec3 c;
     vec3 tex = t1;
@@ -19,11 +18,9 @@ void main () {
     
     points[0]=p0.xy; points[1]=p1.xy; points[2]=p2.xy; points[3]=p3.xy; points[4]=p4.xy;
     
-    float sinNorm = sinN(time/7.) + 0.1;
     
     // stN = rotate(stN, vec2(0.5, 0.5), time/200.);
     
-    float numDiv = 10. * sinNorm;
     vec2 xy = stN;
     
     float timeSpeed = 10.;
@@ -33,20 +30,18 @@ void main () {
     float timeInd = floor(time*60.);
     
     for(int i = 0; i < 5; i++){
+        float sinNorm = sinN(time/(3.+float(i)) + float(i))/2. + 0.1;
         vec2 center = vec2(nn(points[i].x), nn(points[i].y));
-        if(distance(stN, center) < 0.1) {
+        if(distance(stN, center) < sinNorm) {
             if(mod(float(i), 2.) == 0.) xy = rotate(stN, center, time/timeSpeed);
             else xy = rotate(stN, center, - time/timeSpeed);
         } 
-    }
-    
-    if(mod(timeInd, 1.) == 0.){ 
         vec3 bbr = texture2D(backbuffer, xy).rgb;
         c = bbr;
-    } else {
-        c = bb;
+        c = mix(c, tex, sinN(time/3. + float(i))/2.);
     }
-    c = mix(c, tex, sinN(time)/2.);
+    
+    
     // c = tex;
     gl_FragColor = vec4(c, 1);
 }
