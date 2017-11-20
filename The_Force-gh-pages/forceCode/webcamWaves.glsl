1247a28d3e252e000cc78581fbe4397744e45ed7
Avneesh Sarwate <avneeshsarwate@gmail.com>
2017-11-20 03:27:39 -0500

    clearer to see what's going on

--- The_Force-gh-pages/forceCode/webcamWaves.glsl
+++ The_Force-gh-pages/forceCode/webcamWaves.glsl
@@ -11,6 +11,12 @@ vec3 quant(vec3 num, float quantLevels){
     return (floor(num*quantLevels)+roundPart)/quantLevels;
 }
 
+float quant(float num, float quantLevels){
+    float roundPart = floor(fract(num*quantLevels)*2.);
+    return (floor(num*quantLevels)+roundPart)/quantLevels;
+}
+
+
 float wrap(float val, float low, float high){
     if(val < low) return low + (low-val);
     if(val > high) return high - (val - high);
@@ -19,7 +25,7 @@ float wrap(float val, float low, float high){
 
 void main() {
     vec2 stN = uvN();
-    vec2 xy = vec2(wrap(stN.x + sin(stN.x*time*4.)*0.05, 0., 1.), wrap(stN.y + cos(stN.y+time*2.)*0.12, 0., 1.));
+    vec2 xy = vec2(wrap(stN.x + sin(quant(stN.x, 10.)*time*8.)*0.05, 0., 1.), wrap(stN.y + cos(stN.y+time*2.)*0.12, 0., 1.));
     vec3 cam = texture2D(channel0, vec2(1.-xy.x, xy.y)).xyz;
     vec3 bb = texture2D(backbuffer, xy).xyz;
     vec2 center = vec2(0.5, 0.5);
@@ -28,5 +34,5 @@ void main() {
     vec3 colorPowerWave = vec3(pow(lumWave.x, sin(time/2.)), pow(lumWave.y, sin(time)), pow(lumWave.z, sin(time/3.)));
     vec3 colorMultWave = vec3(lumWave.x * sinN(time/2.), lumWave.y * sinN(time), lumWave.z * sinN(time/3.));
     vec3 c = mix(colorWave, lumWave, sin(time));
-	gl_FragColor = vec4(lumWave * colorPowerWave, 1.0);
+    gl_FragColor = vec4(lumWave * colorPowerWave, 1.0);
 }
