c3c5951caa7e9783d7558a8c8cb7cf253a268382
Avneesh Sarwate <avneeshsarwate@gmail.com>
2017-12-01 15:16:36 -0500

    cleanup

--- The_Force-gh-pages/forceCode/webcamDiffSinExperiments.glsl
+++ The_Force-gh-pages/forceCode/webcamDiffSinExperiments.glsl
@@ -14,13 +14,8 @@ float cosN(float t){
    return (cos(t) + 1.) / 1.; 
 }
 
-void main () {
+vec3 diffColor(){
     vec2 stN = uvN();
-    vec3 snap = texture2D(channel3, vec2(1. -stN.x, 1. - stN.y)).rgb;
-    vec3 cam = texture2D(channel0, vec2(1. -stN.x, 1.-stN.y)).rgb;
-    vec3 camflip = texture2D(channel0, vec2(stN.x, stN.y)).rgb;
-    vec3 tex = texture2D(channel1, vec2(stN.x, 1.-stN.y)).rgb;
-    
     stN = rotate(vec2(0.5+sin(time)*0.5, 0.5+cos(time)*0.5), stN, sin(time));
     
     vec2 segGrid = vec2(floor(stN.x*30.0 * sin(time/7.)), floor(stN.y*30.0 * sin(time/7.)));
@@ -30,20 +25,25 @@ void main () {
     if(mod(segGrid.x, 2.) == mod(segGrid.y, 2.)) xy = rotate(vec2(sinN(time), cosN(time)), stN.xy, time + noiseVal);
     else xy = rotate(vec2(sinN(time), cosN(time)), stN.xy, - time - noiseVal);
     
-    float section = floor(xy.x*30.0 * sin(time/7.));
+    float section = floor(xy.x*30.0 * sin(time/7.)); 
     float tile = mod(section, 2.);
 
-    float section2 = floor(xy.y*30.0 * cos(time/7.));
+    float section2 = floor(xy.y*30.0 * cos(time/7.)); 
     float tile2 = mod(section2, 2.);
-    
     float timeMod = time - (1. * floor(time/1.));
-    vec3 col = vec3(tile, tile2, timeMod);
-    col = mix(col, cam, sinN(time/4.));
     
+    return vec3(tile, tile2, timeMod);
+}
+
+void main () {
+    vec2 stN = uvN();
+    vec3 snap = texture2D(channel3, vec2(1. -stN.x, 1. - stN.y)).rgb;
+    vec3 cam = texture2D(channel0, vec2(1. -stN.x, 1.-stN.y)).rgb;
     
-    
+    vec3 col = diffColor();
+
     vec3 c;
     if(colourDistance(cam, snap) < .5) c = cam; 
     else c = col;
-	gl_FragColor = vec4(c, 1.0);
+    gl_FragColor = vec4(c, 1.0);
 }
