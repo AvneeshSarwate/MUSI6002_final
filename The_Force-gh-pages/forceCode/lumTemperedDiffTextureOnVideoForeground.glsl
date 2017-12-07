2ab858f0a247ec993c3e95a5756d40c618924deb
Avneesh Sarwate <avneeshsarwate@gmail.com>
2017-12-07 11:54:04 -0500

    starting to parameterize shader for mouse input

--- The_Force-gh-pages/forceCode/lumTemperedDiffTextureOnVideoForeground.glsl
+++ The_Force-gh-pages/forceCode/lumTemperedDiffTextureOnVideoForeground.glsl
@@ -14,23 +14,23 @@ float cosN(float t){
    return (cos(t) + 1.) / 1.; 
 }
 
-vec3 diffColor(){
+vec3 diffColor(float time2){
     vec2 stN = uvN();
-    stN = rotate(vec2(0.5+sin(time)*0.5, 0.5+cos(time)*0.5), stN, sin(time));
+    stN = rotate(vec2(0.5+sin(time2)*0.5, 0.5+cos(time2)*0.5), stN, sin(time2));
     
-    vec2 segGrid = vec2(floor(stN.x*30.0 * sin(time/7.)), floor(stN.y*30.0 * sin(time/7.)));
+    vec2 segGrid = vec2(floor(stN.x*30.0 * sin(time2/7.)), floor(stN.y*30.0 * sin(time2/7.)));
 
     vec2 xy;
-    float noiseVal = rand(stN)*sin(time/7.) * 0.15;
-    if(mod(segGrid.x, 2.) == mod(segGrid.y, 2.)) xy = rotate(vec2(sinN(time), cosN(time)), stN.xy, time + noiseVal);
-    else xy = rotate(vec2(sinN(time), cosN(time)), stN.xy, - time - noiseVal);
+    float noiseVal = rand(stN)*sin(time2/7.) * 0.15;
+    if(mod(segGrid.x, 2.) == mod(segGrid.y, 2.)) xy = rotate(vec2(sinN(time2), cosN(time2)), stN.xy, time2 + noiseVal);
+    else xy = rotate(vec2(sinN(time2), cosN(time2)), stN.xy, - time2 - noiseVal);
     
-    float section = floor(xy.x*30.0 * sin(time/7.)); 
+    float section = floor(xy.x*30.0 * sin(time2/7.)); 
     float tile = mod(section, 2.);
 
-    float section2 = floor(xy.y*30.0 * cos(time/7.)); 
+    float section2 = floor(xy.y*30.0 * cos(time2/7.)); 
     float tile2 = mod(section2, 2.);
-    float timeMod = time - (1. * floor(time/1.)); 
+    float timeMod = time2 - (1. * floor(time2/1.)); 
     
     return vec3(tile, tile2, timeMod);
 }
@@ -58,27 +58,35 @@ float wrap(float val, float low, float high){
     return val;
 }
 
-vec3 block() {
+float bound(float val, float lower, float upper){
+    return max(lower, min(upper, val));
+}
+
+bool boxHasColorDiff(vec2 xy, float boxSize, float diffThresh, float diffFrac){
+    float boxArea = boxSize * boxSize;
+    return true;
+}
+
+float block(float numBlocks, float quantLevel) {
     vec2 stN = uvN();
     vec3 cam = texture2D(channel0, vec2(1.-stN.x, stN.y)).xyz; 
     vec3 lumC = lum(cam);
-    float numBlocks = 50.+ sinN(time/2.) * 40.;
     vec2 res = gl_FragCoord.xy / stN;
     vec2 blockSize = res.xy / numBlocks;
     vec2 blockStart = floor(gl_FragCoord.xy / blockSize) * blockSize / res.xy;
-    vec3 blockAvgLuma = vec3(0.);
+    float blockAvgLuma = 0.;
     vec2 counter = blockStart;
     
     vec2 inc = vec2(1. / (numBlocks *100.));
     for(float i = 0.; i < 10.; i += 1.){
         for(float j = 0.; j < 10.; j += 1.){
-            blockAvgLuma += lum(texture2D(channel0, vec2(1.-counter.x, counter.y)).xyz);
+            blockAvgLuma += lum(texture2D(channel0, vec2(1.-counter.x, counter.y)).xyz).r;
             counter += inc;
         }
     }
     blockAvgLuma /= 100.;
     
-    return quant(blockAvgLuma, 7.+sinN(time/1.5)*10.);
+    return quant(blockAvgLuma, quantLevel);
 }
 
 void main () {
@@ -87,18 +95,19 @@ void main () {
     vec3 cam = texture2D(channel0, vec2(1. -stN.x, stN.y)).rgb;  
     vec3 bb = texture2D(backbuffer, vec2(stN.x, stN.y)).rgb;
     vec3 t1 = texture2D(channel1, vec2(1. -stN.x, stN.y)).rgb;  
-    vec3 col = diffColor();
+    vec3 col = diffColor(time/5.);
 
     vec3 c;
     float lastFeedback = texture2D(backbuffer, vec2(stN.x, stN.y)).a; 
     float feedback; 
     float decay = 0.975;
-    vec3 blockColor = block();
+    float blockColor = block(50.+ sinN(time/2.) * 40., 7.+sinN(time/1.5)*10.) + 0.01;
+    float lumBlend = 0.25;
     
     if(colourDistance(cam, snap) > 0.6){
         if(lastFeedback < 1.) {
             feedback = 1.;
-            c = col * sqrt(sqrt(blockColor));
+            c = col * pow(blockColor, lumBlend);
         } else {
             feedback = lastFeedback * decay;
             c = mix(snap, bb, lastFeedback);
@@ -107,7 +116,7 @@ void main () {
     else {
         feedback = lastFeedback * decay;
         if(lastFeedback > 0.5) {
-            c = mix(snap, col * sqrt(sqrt(blockColor)), lastFeedback); //swap col for bb for glitchier effect
+            c = mix(snap, col * pow(blockColor, lumBlend), lastFeedback); //swap col for bb for glitchier effect
         } else {
             c = t1;
         }
